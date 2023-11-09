4d6d29d29179dc7f13db2b25754add1f306deba5
chris1797 <jaehun6004@gmail.com>
2023-11-10 00:01:54 +0900

    init

---
+++ src/main/java/com/base/jooq/JooqBaseApplication.java
@@ -0,0 +1,13 @@
+package com.base.jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/base/jooq/JooqBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.jooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
