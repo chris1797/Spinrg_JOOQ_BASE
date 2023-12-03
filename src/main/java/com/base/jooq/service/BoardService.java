a0706a607cd7cda65c6708547ac6792035f03b2a
chris1797 <jaehun6004@gmail.com>
2023-12-04 00:25:47 +0900

    setup

--- src/main/java/com/base/jooq/service/BoardService.java
+++ src/main/java/com/base/jooq/service/BoardService.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class)
-public class BoardService {
+public class BoardService extends BaseService {
 
     private final BoardDao dao;
 
