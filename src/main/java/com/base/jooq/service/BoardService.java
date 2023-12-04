f7adc6f0535598d5a175471402f611c94d0c2f68
chris1797 <jaehun6004@gmail.com>
2023-12-04 23:22:39 +0900

    commit

--- src/main/java/com/base/jooq/service/BoardService.java
+++ src/main/java/com/base/jooq/service/BoardService.java
@@ -1,14 +1,17 @@
 package com.base.jooq.service;
 
 import com.base.jooq.jooq.bean.tables.pojos.Tblboard;
+import com.base.jooq.jooq.bean.tables.records.TblboardRecord;
 import com.base.jooq.jooq.dao.BoardDao;
 import com.base.jooq.jooq.dto.request.BoardPageReq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -17,6 +20,7 @@ import java.util.List;
 public class BoardService extends BaseService {
 
     private final BoardDao dao;
+    private final ModelMapper mapper;
 
     public List<Tblboard> getAllBoard(BoardPageReq req) {
         /**
@@ -29,5 +33,16 @@ public class BoardService extends BaseService {
     public Tblboard getBoard(Long boardNo) throws Exception{
         return dao.getBoardByNo(boardNo).orElseThrow(() ->
                 new Exception("This data could not be found."));
+    }
+
+    public boolean save(Tblboard entity) throws Exception {
+        if (Objects.isNull(entity)) throw new Exception("This data is null.");
+        TblboardRecord record = mapper.map(entity, TblboardRecord.class);
+        return dao.save(record);
+    }
+
+    public boolean remove(Long boardNo) throws Exception {
+        if (Objects.isNull(boardNo) || boardNo < 1) throw new Exception("This data could not be found.");
+        return dao.remove(boardNo);
     }
 }
