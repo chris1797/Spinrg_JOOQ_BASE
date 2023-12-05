645f95d10e345ef87e68c4c3f9aad226e8818f1e
chris1797 <jaehun6004@gmail.com>
2023-12-06 00:06:13 +0900

    setup BeanConfig - ModelMapper

---
+++ src/main/java/com/base/jooq/config/BeanConfig.java
@@ -0,0 +1,27 @@
+package com.base.jooq.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration annotation은 Bean을 수동으로 등록하기 위한 설정 파일이라고 명시
+ * Confituration의 역할은 스프링 컨테이너에서 이 안에 등록된 Bean들을 관리할 수 있게 하고
+ * 해당 Bean들을 싱글톤으로 유지
+ */
+@Configuration(value = "config")
+public class BeanConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true) // PRIVATE Field가 일치되도록 설정
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+
+        return modelMapper;
+    }
+}
